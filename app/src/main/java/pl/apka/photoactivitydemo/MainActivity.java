7f90df5389e85693ad37c07d65d1ea4fec0bc01a
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-18 19:52:08 +0100

    onActivityResult

--- app/src/main/java/pl/apka/photoactivitydemo/MainActivity.java
+++ app/src/main/java/pl/apka/photoactivitydemo/MainActivity.java
@@ -1,11 +1,18 @@
 package pl.apka.photoactivitydemo;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,26 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent,1);
 
-        
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri selectedImage = data.getData();
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                imageView = findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
 
     }
 }
