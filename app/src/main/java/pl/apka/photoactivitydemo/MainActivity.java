4fcdf2cea5cbf4bb4bc5864cf6e869b103314853
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-18 18:30:40 +0100

    image view

--- app/src/main/java/pl/apka/photoactivitydemo/MainActivity.java
+++ app/src/main/java/pl/apka/photoactivitydemo/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent,1);
+
         
+
     }
 }
